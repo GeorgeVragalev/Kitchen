49cb91c82e42cbd9f4e2c939f64dbac3f188cd2f
George Vragalev <78079918+GeorgeVragalev@users.noreply.github.com>
2022-09-10 13:21:49 +0300

    gitignore. OrderController

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using DiningHall.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiningHall.Controllers;
+
+[ApiController]
+[Route("api/controller/order")]
+public class OrderController : ControllerBase
+{
+    [HttpPost]
+    public ActionResult Index()
+    {
+        var order = new Order()
+        {
+            Id = 1,
+            Foods = new List<int>() {1, 2, 4},
+            MaxWait = 30,
+            TableId = 2,
+            WaiterId = 1
+        };
+        
+        return new JsonResult(order);
+    }
+    
+    [HttpGet]
+    public ActionResult Get()
+    {
+        var order = new Order()
+        {
+            Id = 2,
+            Foods = new List<int>() {4},
+            MaxWait = 3,
+            TableId = 1,
+            WaiterId = 1
+        };
+        
+        return new JsonResult(order);
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace DiningHall.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public int TableId { get; set; }
+    public int WaiterId { get; set; }
+    public int MaxWait { get; set; }
+    public IList<int> Foods { get; set; }
+
+    public Order()
+    {
+    }
+}
